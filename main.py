514ec8f6c77a2921b28ab4b148497669a9d1c920
Jack Dunlavey <dunlavey2003@gmail.com>
2023-01-13 14:34:09 +0000

    Commented functions.

--- main.py
+++ main.py
@@ -13,13 +13,21 @@ def main():
         restaurant_table_object = TableObject(table_number)
         restaurant_table_object.action_handler()
 
-
 def create_tables_directory():
+    """
+    Creates a 'table' directory if it doesn't already exit.
+    """
     if not os.path.exists('tables'):
         os.mkdir('tables')
 
 
-def validate_table_number(max_table_number) -> str:
+def validate_table_number(max_table_number: int) -> str:
+    """
+    Returns a valid integer as a string.
+    :param max_table_number:
+    :return: table_number
+    :rtype: str
+    """
     while True:
         table_number: str = input(f'Enter the Table Number (1 to {max_table_number}), or "{ESCAPE_PHRASE}" to end: ')
         if table_number.isdigit():
@@ -35,11 +43,17 @@ class TableObject:
         self.___create_csv_file()
 
     def ___create_csv_file(self):
+        """
+        Creates a corresponding csv file if it doesn't already exist.
+        """
         if not os.path.exists(self.csv_url):
             with open(self.csv_url, 'x') as new_file:
                 new_file.write('Item,Price')
 
     def action_handler(self):
+        """
+        Public method to access other private methods.
+        """
         while True:
             choice = input(f'Enter an action (Append, Display, Clear), or "{ESCAPE_PHRASE}" to go back: ')
             if choice.lower() == ESCAPE_PHRASE:
@@ -56,7 +70,7 @@ class TableObject:
 
     def ___append_new_items_into_a_dictionary(self):
         print(menu := pd.read_csv(MENU_URL))
-        items_to_append = {'Item': [], 'Price': []}
+        items_to_append: dict = {'Item': [], 'Price': []}
         while True:
             desired_item = input(f'Enter an item from the menu, or "{ESCAPE_PHRASE}" to go back: ')
             if desired_item == ESCAPE_PHRASE:
@@ -74,6 +88,11 @@ class TableObject:
 
     @staticmethod
     def ___append_get_quantity() -> int:
+        """
+        Gets the user's desired quantity for one type of item on the menu.
+        :return: quantity
+        :rtype: int
+        """
         while True:
             quantity = input("Enter Quantity (0 to cancel): ")
             if quantity.isdigit():
@@ -86,6 +105,9 @@ class TableObject:
         df_combined_entries.to_csv(self.csv_url, mode='w', index=False)
 
     def ___display_csv_file_contents(self):
+        """
+        Displays the contents of csv file, unless the file as no entries.
+        """
         if (csv_contents := pd.read_csv(self.csv_url)).empty:
             print('This table has no ordered items!')
             return
@@ -93,6 +115,9 @@ class TableObject:
         print(f'Total: {sum(csv_contents["Price"])}')
 
     def ___clear_contents(self):
+        """
+        Resets csv file to only its headers.
+        """
         with open(self.csv_url, 'w') as new_file:
             new_file.write('Item,Price')
 
